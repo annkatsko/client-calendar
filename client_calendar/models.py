387bea80161dc0f9785fdc8a01c7f68cf6482414
Hanna Katsko <1187484@gmail.com>
2022-10-28 12:32:37 +0300

    added model Session

--- client_calendar/models.py
+++ client_calendar/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from registration.models import Profile
 # Create your models here.
+
+class Session(models.Model):
+    GYM_NAME = (
+        ('NB', 'Новая Боровая "Vibe"'),
+        ('NEM', 'Немига "GymBox"')
+    )
+
+    date = models.DateField('Дата тренировки', blank=False)
+    created_date = models.DateField(auto_now=True, blank=False)
+    session_time = models.TimeField('Время тренировки', blank=True, null=True)
+    gym_name = models.CharField('Зал', choices=GYM_NAME, blank=False, max_length=50, default='Новая Боровая "Vibe')
+    client = models.ForeignKey(Profile, on_delete=models.PROTECT, verbose_name='client_id')
--- configs/urls.py
+++ configs/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('welcome/', include('registration.urls')),
     path('homepage/', include('home_page.urls')),
+    path('calendar/', include('client_calendar.urls')),
 ]
