e8526e905c41a26ee8a184c4d30765c6664087b6
Hanna Katsko <1187484@gmail.com>
2022-12-14 16:53:40 +0300

    profile form validation added

--- home_page/views.py
+++ home_page/views.py
@@ -11,7 +11,7 @@ def view_homepage(request):
 def view_and_edit_profile(request):
     if request.method == 'POST':
         user_form = UserEditForm(instance=request.user, data=request.POST)
-        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
--- registration/forms.py
+++ registration/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from .models import Profile
-
+import phonenumbers
 
 class UserRegistrationForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -36,6 +36,27 @@ class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['gym_name', 'phone_number', 'birthday_date', 'instagram', 'telegram', 'goal']
+        help_texts = {'birthday_date': 'Введите дату в формате день.месяц.год',
+                      'instagram': 'Введите ник без символа "@" или ссылку на Ваш профиль.',
+                      'telegram': 'Введите ник без символа "@" или ссылку на Ваш профиль.',
+                      'phone_number': 'Введите номер телефона в формате +375...'
+                      }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        user_instagram = cleaned_data['instagram']
+        user_telegram = cleaned_data['telegram']
+        string_user_phone = cleaned_data['phone_number']
+        parsed_user_phone = phonenumbers.parse(string_user_phone, 'BY')
+        if '@' in user_instagram or '@' in user_telegram:
+            raise forms.ValidationError('Введите ник без "@"')
+        if not phonenumbers.is_valid_number(parsed_user_phone):
+            raise forms.ValidationError('Введите корректный номер телефона.')
+
+
+
+
+
 
 
 
